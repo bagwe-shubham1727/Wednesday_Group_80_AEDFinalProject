d8d327f7f8bb8832581bee71975e49b2d4f9d287
Shubham Bagwe <bagwe.sh@northeastern.edu>
2024-11-27 21:47:28 -0500

    Initital packages

---
+++ UniNest/src/emergencyEnterprise/emergency/Model/Police.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package emergencyEnterprise.emergency.Model;
+
+/**
+ *
+ * @author suhas
+ */
+public class Police {
+    
+}
